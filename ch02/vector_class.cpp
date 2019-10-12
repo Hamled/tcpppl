81aea5b961692af40594e2d6294943ef870ee07f
Charles Ellis <hamled@hamled.name>
2019-10-11 23:27:44 -0700

    exception throwing and handling example

--- ch02/vector_class.cpp
+++ ch02/vector_class.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 class Vector {
 public:
-  Vector(int s) : elem{new double[s]}, sz{s} {} // construct a Vector
-  double& operator[](int i) { return elem[i]; } // element access: subscripting
+  Vector(int s) : sz{s} { // construct a Vector
+    if(sz < 0) throw length_error{"Vector::Vector"};
+    elem = new double[sz];
+  }
+  double& operator[](int i) { // element access: subscripting
+    if(i < 0 || size() <= i) throw out_of_range{"Vector::operator[]"};
+    return elem[i];
+  }
   int size() { return sz; }
 private:
   double* elem; // pointer to the elements
@@ -29,6 +35,14 @@ int main() {
   int s;
   cin >> s;
 
-  double sum = read_and_sum(s);
-  cout << "\nSum: " << sum << "\n";
+  try {
+    double sum = read_and_sum(s);
+    cout << "\nSum: " << sum << "\n";
+  } catch(length_error) {
+    cout << "You must provide a non-negative quantity of numbers.\n";
+    return 1;
+  } catch(bad_alloc) {
+    cout << "Unable to allocate memory for numbers.\n";
+    return 2;
+  }
 }
